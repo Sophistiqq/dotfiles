c4e2184c3216d682f23b7ef0001f3b746163a1ae
Roi Borromeo <roi.for.school@gmail.com>
2024-08-12 17:02:04 +0800

    New repo

---
+++ .config/nvim/lua/chadrc.lua
@@ -0,0 +1,39 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+  theme = "everforest",
+  cmp = {
+    style = "default"
+  },
+  hl_override = {
+  	Comment = { italic = true },
+  	["@comment"] = { italic = true },
+  },
+  term = {
+    hl = "Normal:term,WinSeparator:WinSeparator",
+    sizes = { sp = 0.5, vsp = 0.5 },
+    float = {
+      relative = "editor",
+      row = 0.3,
+      col = 0.25,
+      width = 0.5,
+      height = 0.4,
+      border = "single",
+    },
+  },
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "arrow",
+    order = nil,
+    modules = nil,
+  },
+
+}
+
+return M
---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          auto_trigger = true,
+          enabled = true,
+          debounce = 75,
+          keymap = {
+            accept = "<M-CR>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        }
+      })
+    end
+  }
+}
