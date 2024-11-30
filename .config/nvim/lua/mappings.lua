e8ab5ef0673cfa02734332153b6bbb3272ef94f8
Roi Borromeo <roi.for.school@gmail.com>
2024-11-30 14:23:16 +0000

    Reinstallation

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,8 +6,5 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-map("n", "<C-Left>", "<C-w>h", { desc = "switch window left" })
-map("n", "<C-Right>", "<C-w>l", { desc = "switch window right" })
-map("n", "<C-Down>", "<C-w>j", { desc = "switch window down" })
-map("n", "<C-Up>", "<C-w>k", { desc = "switch window up" })
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
