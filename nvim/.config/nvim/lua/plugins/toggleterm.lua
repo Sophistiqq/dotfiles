0abcdb0f1473a481c34523868699eae6068b2fe3
Roi Borromeo <92098998+Sophistiqq@users.noreply.github.com>
2025-12-27 15:22:24 +0800

    Changed from toggleterm to floaterm

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -1,13 +1,6 @@
 vim.pack.add({
-  { src = "https://github.com/akinsho/toggleterm.nvim" }
+  { src = "https://github.com/nvzone/floaterm.git" }
 })
 
-require("toggleterm").setup({
+require("floaterm").setup({
 })
-
--- exit terminal mode
-vim.keymap.set("t", "<C-x>", [[<C-\><C-n>]], { silent = true })
-
-vim.keymap.set({ "n", "t" }, "<M-v>", [[<C-\><C-n>:ToggleTerm direction=vertical size=50<CR>]], { silent = true })
-vim.keymap.set({ "n", "t" }, "<M-h>", [[<C-\><C-n>:ToggleTerm direction=horizontal<CR>]], { silent = true })
-vim.keymap.set({ "n", "t" }, "<M-i>", [[<C-\><C-n>:ToggleTerm direction=float<CR>]], { silent = true })
